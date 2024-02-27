1bd89295ac23808ab5f4df7871f214f736ea3ff1
Ignacio Heck <ignaciojuanheck@gmail.com>
2024-02-27 14:29:02 -0300

    client: add SelectAndCreate component

--- client/src/app/altaLote/page.tsx
+++ client/src/app/altaLote/page.tsx
@@ -1,6 +1,9 @@
+'use client';
+
 import closeSvg from '../../../public/x-symbol-svgrepo-com.svg';
 import newItemSvg from '../../../public/new-svgrepo-com.svg';
 import Image from 'next/image';
+import { SelectAndCreate } from '@/components/selectAndCreate/SelectAndCreate';
 
 export default function AltaLote() {
   return (
@@ -9,24 +12,22 @@ export default function AltaLote() {
         <h1 className='text-2xl w-full pt-12 font-bold'>Alta Lote</h1>
         <div className='divider bg-white h-[1px]'></div>
         <div className='flex gap-2 w-full'>
-          <select className='select w-full'>
-            <option value=''>Seleccionar odontologo...</option>
-          </select>
-          <button className='btn'>
-            <Image src={newItemSvg} width={18} alt='delete'></Image>
-          </button>
+          <SelectAndCreate
+            newItemImg={newItemSvg}
+            options={['Odontologo 1', 'Odontologo 2', 'Odontologo 3']}
+            defaultOptionName='Seleccionar odontologo...'
+            onSelect={() => {}}
+          ></SelectAndCreate>
         </div>
         <div className='flex gap-4 w-full items-center'>
           <div className='flex gap-2 flex-col w-full'>
             <label>Obra social</label>
-            <div className='flex gap-2'>
-              <select className='select w-full'>
-                <option value=''>Seleccionar obra social...</option>
-              </select>
-              <button className='btn'>
-                <Image src={newItemSvg} width={18} alt='delete'></Image>
-              </button>
-            </div>
+            <SelectAndCreate
+              newItemImg={newItemSvg}
+              options={['OSDE', 'Swiss Medical', 'Galeno']}
+              defaultOptionName='Seleccionar obra social...'
+              onSelect={() => {}}
+            ></SelectAndCreate>
           </div>
           <div className='flex'>
             <div className='flex flex-col gap-2'>
@@ -42,82 +43,91 @@ export default function AltaLote() {
           Bonos por obra social
         </h2>
         <div className='w-full overflow-y-auto h-96'>
-          <table className='table'>
+          <table className='table table-pin-rows'>
             <thead>
               <tr>
+                <th>#</th>
                 <th>Obra social</th>
                 <th>Cantidad</th>
-                <th>Acciones</th>
+                <th className='w-24'>Acciones</th>
               </tr>
             </thead>
             <tbody>
               <tr>
+                <td>1</td>
                 <td>OSDE</td>
                 <td>5</td>
-                <td>
+                <td className='flex justify-center'>
                   <button className='btn btn-sm btn-square btn-error'>
                     <Image src={closeSvg} width={12} alt='delete'></Image>
                   </button>
                 </td>
               </tr>
               <tr>
+                <td>2</td>
                 <td>Swiss Medical</td>
                 <td>3</td>
-                <td>
+                <td className='flex justify-center'>
                   <button className='btn btn-sm btn-square btn-error'>
                     <Image src={closeSvg} width={12} alt='delete'></Image>
                   </button>
                 </td>
               </tr>
               <tr>
+                <td>3</td>
                 <td>Swiss Medical</td>
                 <td>3</td>
-                <td>
+                <td className='flex justify-center'>
                   <button className='btn btn-sm btn-square btn-error'>
                     <Image src={closeSvg} width={12} alt='delete'></Image>
                   </button>
                 </td>
               </tr>
               <tr>
+                <td>4</td>
                 <td>Swiss Medical</td>
                 <td>3</td>
-                <td>
+                <td className='flex justify-center'>
                   <button className='btn btn-sm btn-square btn-error'>
                     <Image src={closeSvg} width={12} alt='delete'></Image>
                   </button>
                 </td>
               </tr>
               <tr>
+                <td>5</td>
                 <td>Swiss Medical</td>
                 <td>3</td>
-                <td>
+                <td className='flex justify-center'>
                   <button className='btn btn-sm btn-square btn-error'>
                     <Image src={closeSvg} width={12} alt='delete'></Image>
                   </button>
                 </td>
               </tr>
               <tr>
+                <td>6</td>
                 <td>Swiss Medical</td>
                 <td>3</td>
-                <td>
+                <td className='flex justify-center'>
                   <button className='btn btn-sm btn-square btn-error'>
                     <Image src={closeSvg} width={12} alt='delete'></Image>
                   </button>
                 </td>
               </tr>
               <tr>
+                <td>7</td>
                 <td>Swiss Medical</td>
                 <td>3</td>
-                <td>
+                <td className='flex justify-center'>
                   <button className='btn btn-sm btn-square btn-error'>
                     <Image src={closeSvg} width={12} alt='delete'></Image>
                   </button>
                 </td>
               </tr>
               <tr>
+                <td>8</td>
                 <td>Swiss Medical</td>
                 <td>3</td>
-                <td>
+                <td className='flex justify-center'>
                   <button className='btn btn-sm btn-square btn-error'>
                     <Image src={closeSvg} width={12} alt='delete'></Image>
                   </button>
---
+++ client/src/components/selectAndCreate/SelectAndCreate.tsx
@@ -0,0 +1,32 @@
+'use client';
+import Image from 'next/image';
+
+export const SelectAndCreate = ({
+  newItemImg,
+  options,
+  defaultOptionName,
+  onSelect,
+}: {
+  newItemImg: any;
+  options: string[];
+  defaultOptionName: string;
+  onSelect: () => void;
+}) => {
+  return (
+    <div className='flex gap-2 w-full'>
+      <select onChange={onSelect} className='select w-full'>
+        <option selected value={''}>
+          {defaultOptionName}
+        </option>
+        {options.map((option: string, key: number) => (
+          <option key={key} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+      <button className='btn btn-square'>
+        <Image src={newItemImg} width={18} alt='newItem'></Image>
+      </button>
+    </div>
+  );
+};
