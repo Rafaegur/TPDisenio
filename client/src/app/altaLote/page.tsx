5ce79a9371adf02becd5d5ab4d4a316507497e2c
Ignacio Heck <ignaciojuanheck@gmail.com>
2024-02-27 14:05:44 -0300

    client: add svg, improve styles, mock data table

--- client/src/app/altaLote/page.tsx
+++ client/src/app/altaLote/page.tsx
@@ -1,14 +1,20 @@
+import closeSvg from '../../../public/x-symbol-svgrepo-com.svg';
+import newItemSvg from '../../../public/new-svgrepo-com.svg';
+import Image from 'next/image';
+
 export default function AltaLote() {
   return (
     <div className='flex w-full h-screen items-center justify-center'>
       <div className='flex flex-col w-1/3 h-full items-center gap-2'>
-        <h1 className='text-2xl w-full pt-3'>Alta Lote</h1>
-        <div className='divider divider-accent'></div>
+        <h1 className='text-2xl w-full pt-12 font-bold'>Alta Lote</h1>
+        <div className='divider bg-white h-[1px]'></div>
         <div className='flex gap-2 w-full'>
           <select className='select w-full'>
             <option value=''>Seleccionar odontologo...</option>
           </select>
-          <button className='btn'>+</button>
+          <button className='btn'>
+            <Image src={newItemSvg} width={18} alt='delete'></Image>
+          </button>
         </div>
         <div className='flex gap-4 w-full items-center'>
           <div className='flex gap-2 flex-col w-full'>
@@ -17,7 +23,9 @@ export default function AltaLote() {
               <select className='select w-full'>
                 <option value=''>Seleccionar obra social...</option>
               </select>
-              <button className='btn'>+</button>
+              <button className='btn'>
+                <Image src={newItemSvg} width={18} alt='delete'></Image>
+              </button>
             </div>
           </div>
           <div className='flex'>
@@ -30,8 +38,95 @@ export default function AltaLote() {
             </div>
           </div>
         </div>
-        <h2 className='w-full'>Bonos por obra social</h2>
-        <table></table>
+        <h2 className='w-full mt-10 text-xl font-semibold'>
+          Bonos por obra social
+        </h2>
+        <div className='w-full overflow-y-auto h-96'>
+          <table className='table'>
+            <thead>
+              <tr>
+                <th>Obra social</th>
+                <th>Cantidad</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>OSDE</td>
+                <td>5</td>
+                <td>
+                  <button className='btn btn-sm btn-square btn-error'>
+                    <Image src={closeSvg} width={12} alt='delete'></Image>
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td>Swiss Medical</td>
+                <td>3</td>
+                <td>
+                  <button className='btn btn-sm btn-square btn-error'>
+                    <Image src={closeSvg} width={12} alt='delete'></Image>
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td>Swiss Medical</td>
+                <td>3</td>
+                <td>
+                  <button className='btn btn-sm btn-square btn-error'>
+                    <Image src={closeSvg} width={12} alt='delete'></Image>
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td>Swiss Medical</td>
+                <td>3</td>
+                <td>
+                  <button className='btn btn-sm btn-square btn-error'>
+                    <Image src={closeSvg} width={12} alt='delete'></Image>
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td>Swiss Medical</td>
+                <td>3</td>
+                <td>
+                  <button className='btn btn-sm btn-square btn-error'>
+                    <Image src={closeSvg} width={12} alt='delete'></Image>
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td>Swiss Medical</td>
+                <td>3</td>
+                <td>
+                  <button className='btn btn-sm btn-square btn-error'>
+                    <Image src={closeSvg} width={12} alt='delete'></Image>
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td>Swiss Medical</td>
+                <td>3</td>
+                <td>
+                  <button className='btn btn-sm btn-square btn-error'>
+                    <Image src={closeSvg} width={12} alt='delete'></Image>
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td>Swiss Medical</td>
+                <td>3</td>
+                <td>
+                  <button className='btn btn-sm btn-square btn-error'>
+                    <Image src={closeSvg} width={12} alt='delete'></Image>
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
         <div className='flex flex-row-reverse w-full gap-2'>
           <button className='btn'>Cancelar</button>
           <button className='btn'>Aceptar</button>
